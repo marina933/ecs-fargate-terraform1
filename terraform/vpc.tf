baa9b28c854c963eb2681e529c7f9b7a870f5adc
DevopsAnwar <anwartfaisal86@gmail.com>
2024-02-02 21:45:03 -0600

    1st commit

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -6,7 +6,7 @@ module "vite_app_vpc" {
   name = "vite-app-vpc"
   cidr = "10.0.0.0/16"
 
-  azs                     = ["eu-west-1a"]
+  azs                     = ["us-east-1a"]
   public_subnets          = ["10.0.1.0/24"]
   map_public_ip_on_launch = true
 
